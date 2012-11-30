1b71802b601677803ce9b42b158f61b6d6964194
Ray Seyfarth <ray.seyfarth@gmail.com>
2012-11-30 10:33:54 -0600

    Modded terminal window to have a frame

--- terminalwindow.cpp
+++ terminalwindow.cpp
@@ -1,18 +1,34 @@
 #include "terminalwindow.h"
 #include <QKeyEvent>
+#include <QHBoxLayout>
 
 
 TerminalWindow::TerminalWindow(QWidget *parent)
-: QPlainTextEdit(parent)
+: QFrame(parent)
 {
     setObjectName("Terminal");
 
-    //setFrameStyle( QFrame::Box | QFrame::Raised );
-    //setLineWidth(4);
-    //setContentsMargins(10,10,10,10);
+    setFrameStyle( QFrame::Panel | QFrame::Raised );
+    setLineWidth(4);
+    setMidLineWidth(1);
+    setContentsMargins(10,10,10,10);
+
+    QHBoxLayout *layout = new QHBoxLayout;
+
+    edit = new TerminalEdit(this);
+
+    layout->addWidget(edit);
+
+    setLayout(layout);
+
 }
 
-void TerminalWindow::keyPressEvent ( QKeyEvent *event )
+TerminalEdit::TerminalEdit(QWidget *parent)
+: QPlainTextEdit(parent)
+{
+}
+
+void TerminalEdit::keyPressEvent ( QKeyEvent *event )
 {
     //if ( event->matches(QKeySequence::ZoomIn) ) event->ignore();
     QPlainTextEdit::keyPressEvent(event);
