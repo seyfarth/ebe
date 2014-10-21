928d12130fee031031cb9f2b362201d8142e03d2
Ray Seyfarth <ray.seyfarth@gmail.com>
2014-10-20 21:04:03 -0500

    Added framewindow.h and .cpp to git
    Former-commit-id: 6247e19a0c40e8b4740fb372b4ec8e50852e75b4
    Former-commit-id: 3630c5b3fd0c7da64414423a69b30942bb748f78

---
+++ framewindow.cpp
@@ -0,0 +1,496 @@
+#include "framewindow.h"
+#include "datawindow.h"
+#include "settings.h"
+#include "gdb.h"
+#include <QDebug>
+#include <QHeaderView>
+#include <QTableWidget>
+#include <QMenu>
+#include <QTableWidgetItem>
+#include <cstdio>
+
+extern DataWindow *dataWindow;
+extern QMap<FileLine,FrameLimit*> frameLimits;
+QMap<int,FrameItem*> frameItems;
+extern GDB *gdb;
+
+FrameLimit::FrameLimit(int _currPars, int _locals, int _newPars )
+    : currPars(_currPars), locals(_locals), newPars(_newPars)
+{
+}
+
+/*
+ *  Constructor
+ */
+FrameWindow::FrameWindow(QWidget *parent)
+    : QFrame (parent)
+{
+    /*
+     *  For the mainwindow's saveState/restoreState and
+     *  saveGeometry/restoreGeometry to work properly each dock widget and toolbar
+     *  needs a unique object name set.
+     */
+    setObjectName("Stack Frame");
+    limit = 0;
+    rows = 0;
+    buildTable();
+    connect ( this, SIGNAL(requestStack(int)), gdb, SLOT(requestStack(int)) );
+    connect ( gdb, SIGNAL(receiveStack(QStringList)),
+              this, SLOT(receiveStack(QStringList)) );
+}
+
+void FrameWindow::rebuildTable()
+{
+    QTableWidgetItem *name;
+    FrameItem *item;
+#ifdef Q_WS_WIN
+    int local5row;
+    rows = 6;
+    if ( limit->currPars > 4 ) {
+        rows += limit->currPars - 4;
+        if ( rows & 1 ) rows++;
+    }
+    returnRow = rows - 2;
+    local1Row = rows - 3;
+    local5Row = rows;
+
+    if ( limit->locals > 4 ) {
+        rows += limit->locals - 4;
+    }
+    rows += limit->newPars;
+    if ( rows & 1 ) rows++;
+    
+    int oldRows = table->rowCount();
+    table->setRowCount(rows);
+    
+    for (int r = oldRows; r < rows; r++) {
+        frameItems[r] = new FrameItem();
+        name = new QTableWidgetItem(QString(""));
+        table->setItem(r,0,name);
+        item = new FrameItem;
+        item->setValue(0);
+        table->setItem(r,1,item);
+        name = new QTableWidgetItem(QString(""));
+        table->setItem(r,2,name);
+    }
+    if ( limit->currPars > 4 ) {
+        table->item(0,0)->setText("");
+        for ( int i=0; i < limit->currPars-4; i++ ) {
+            table->item(returnRow-i-1,0)->
+                   setText(QString("currPar%1").arg(i+5));
+            table->item(returnRow-i-1,2)->
+                   setText(QString("rbp+%1").arg((i+2)*8));
+        }
+    }
+
+    table->item(returnRow+1,2)->setText("rbp");
+    table->item(returnRow,0)->setText("retAddr");
+    table->item(returnRow,2)->setText("");
+    table->item(returnRow+1,0)->setText("prevRbp");
+    if ( rows > returnRow+2) table->item(returnRow+2,0)->setText("");
+
+    int num=limit->locals;
+    if ( num > 4 ) num = 4;
+
+    for ( int i = 0; i <  num; i++ ) {
+        table->item(local1Row-i,0)->setText(QString("local%1").arg(i+1));
+        table->item(local1Row-i,2)->setText(QString("rbp+%1").arg((i+2)*8));
+    }
+    for ( int i = 4; i <  limit->locals; i++ ) {
+        table->item(local5Row-i,0)->setText(QString("local%1").arg(i+1));
+        table->item(local5Row-i,2)->setText(QString("rbp-%1").arg((i+1)*8));
+    }
+
+    if ( limit->locals > 4 && rows > local5Row+limit->locals-4) {
+        table->item(local5Row+limit->locals-4,0)->setText("");
+    }
+    for ( int i = 5; i <= limit->newPars; i++ ) {
+        table->item(rows-i,0)->setText(QString("newPar%1").arg(i));
+        table->item(rows-i,2)->setText(QString("rsp+%1").arg((i-1)*8));
+    }
+    table->item(rows-1,2)->setText("rsp");
+#else
+    rows = 2;
+    if ( limit->currPars > 6 ) {
+        rows += limit->currPars - 6;
+        if ( rows & 1 ) rows++;
+    }
+    returnRow = rows - 2;
+    rows += limit->locals;
+    local1Row = returnRow+2;
+    if ( limit->newPars > 6 ) rows += limit->newPars - 6;
+    if ( rows & 1 ) rows++;
+    int oldRows = table->rowCount();
+    table->setRowCount(rows);
+    
+    for (int r = oldRows; r < rows; r++) {
+        frameItems[r] = new FrameItem();
+        name = new QTableWidgetItem(QString(""));
+        table->setItem(r,0,name);
+        item = new FrameItem;
+        item->setValue(0);
+        table->setItem(r,1,item);
+        name = new QTableWidgetItem(QString(""));
+        table->setItem(r,2,name);
+    }
+    if ( limit->currPars > 6 ) {
+        table->item(0,0)->setText("");
+        for ( int i=0; i < limit->currPars-6; i++ ) {
+            table->item(returnRow-i-1,0)->
+                   setText(QString("currPar%1").arg(i+7));
+            table->item(returnRow-i-1,2)->
+                   setText(QString("rbp+%1").arg((i+2)*8));
+        }
+    }
+
+    table->item(returnRow+1,2)->setText("rbp");
+    table->item(returnRow,0)->setText("retAddr");
+    table->item(returnRow,2)->setText("");
+    table->item(returnRow+1,0)->setText("prevRbp");
+    if ( rows > returnRow+2) table->item(returnRow+2,0)->setText("");
+    for ( int i = 0; i < limit->locals; i++ ) {
+        table->item(local1Row+i,0)->setText(QString("local%1").arg(i+1));
+        table->item(local1Row+i,2)->setText(QString("rbp-%1").arg((i+1)*8));
+    }
+    if ( rows > local1Row+limit->locals) {
+        table->item(local1Row+limit->locals,0)->setText("");
+    }
+    for ( int i = 7; i <= limit->newPars; i++ ) {
+        table->item(rows-i+6,0)->setText(QString("newPar%1").arg(i));
+        table->item(rows-i+6,2)->setText(QString("rsp+%1").arg((i-7)*8));
+    }
+    if ( limit->locals == 0 && limit->newPars <= 6 ) {
+        table->item(rows-1,2)->setText("rbp, rsp");
+    } else if ( (limit->locals & 1) == 0 && limit->newPars <= 6 ){
+        table->item(rows-1,2)->
+                   setText(QString("rbp-%1, rsp").arg(limit->locals*8));
+    } else {
+        table->item(rows-1,2)->setText("rsp");
+    }
+#endif
+
+    table->resizeColumnsToContents();
+    table->resizeRowsToContents();
+}
+
+void FrameWindow::receiveStack(QStringList results)
+{
+    uLong x;
+    QStringList parts;
+    FrameItem *item;
+    bool ok;
+    int row = rows-1;
+    for ( int i=0; i < results.length(); i++ ) {
+        parts = results[i].split(QRegExp("\\s+"));
+        x = parts[1].toULongLong(&ok,16);
+        item = (FrameItem *)table->item(row,1);
+        item->setValue(x);
+        item->setText(item->value());
+        item->setTextAlignment(Qt::AlignRight | Qt::AlignVCenter);
+        x = parts[2].toULongLong(&ok,16);
+        item = (FrameItem *)table->item(row-1,1);
+        item->setValue(x);
+        item->setText(item->value());
+        item->setTextAlignment(Qt::AlignRight | Qt::AlignVCenter);
+        row -= 2;
+        if ( row < 0 ) break;
+    }
+    table->resizeColumnsToContents();
+    table->resizeRowsToContents();
+}
+
+void FrameWindow::buildTable()
+{
+    /*
+     *  Set the frame to be raised with a width 4 bevel.
+     */
+    setFrameStyle(QFrame::Panel | QFrame::Raised);
+    setLineWidth(4);
+
+    /*
+     *  Create a table to display the registers
+     */
+    table = new QTableWidget(this);
+
+    /*
+     *  We need a layout for the table widget
+     */
+    layout = new QVBoxLayout;
+    layout->setSpacing(5);
+    
+    /*
+     *  Leave 10 pixels all around the table
+     */
+    layout->setContentsMargins(10, 10, 10, 10);
+
+    table->setRowCount(rows);
+    table->setColumnCount(3);
+
+    QTableWidgetItem *name;
+    table->verticalHeader()->hide();
+    table->horizontalHeader()->hide();
+    for (int r = 0; r < rows; r++) {
+        for (int c = 0; c < 3; c++) {
+            name = new QTableWidgetItem(QString("   "));
+            table->setItem(r,c,name);
+        }
+    }
+
+    /*
+     *  Resizing based on size hints which is not too accurate
+     */
+    table->resizeColumnsToContents();
+    table->resizeRowsToContents();
+
+    /*
+     *  Don't show a grid
+     */
+    //table->setShowGrid(false);
+
+    /*
+     *  Set a tooltip to display when the cursor is over the table
+     */
+    table->setToolTip(tr("Right click on quad-word names to change formats.\n"
+        "Right click on a quad-word's value to define a variable\n"
+        "with the address contained in the quad-word."));
+
+    /*
+     *  Add the table to the layout and set the layout for the frame.
+     */
+    layout->addWidget(table);
+    setLayout(layout);
+}
+
+/*
+ *  Crude size hint.  50 rows is not enough to display much, but it allows
+ *  the user to shrink the register window.  300 columns is probably not
+ *  enough.
+ */
+QSize FrameWindow::sizeHint() const
+{
+    return QSize(200, 200);
+}
+
+void FrameWindow::receiveFrame(StringHash)
+{
+}
+
+/*
+ *  This function sets the row heights and column widths based on
+ *  the contents of the table and the current font size.
+ */
+void FrameWindow::setFontHeightAndWidth(int height, int width)
+{
+    fontHeight = height;
+    fontWidth = width;
+    table->resizeColumnsToContents();
+    table->resizeRowsToContents();
+}
+
+/*
+ *  Slot triggered by the gdb class sending a map of register
+ *  values.
+ */
+
+/*
+ *  Virtual function from the QFrame class over-ridden to provide
+ *  popup menus for the register window.
+ */
+void FrameWindow::contextMenuEvent(QContextMenuEvent * /* event */)
+{
+    int column = table->currentColumn();
+    QMenu menu(tr("Frame menu"));
+    if (column % 2 == 0) {
+        /*
+         *      If the column is an even number, this is a register name
+         *      and the menu needs to allow changing formats.
+         */
+        menu.addAction(tr("Decimal format"), this, SLOT(setDecimal()));
+        menu.addAction(tr("Hexadecimal format"), this, SLOT(setHex()));
+        menu.addAction(tr("Double format"), this, SLOT(setDouble()));
+        menu.addAction(tr("Float format"), this, SLOT(setFloat()));
+        menu.addSeparator();
+        menu.addAction(tr("Decimal format - all"), this, SLOT(setDecimalAll()));
+        menu.addAction(tr("Hexadecimal format - all"), this, SLOT(setHexAll()));
+        menu.addAction(tr("Double format - all"), this, SLOT(setDoubleAll()));
+        menu.addAction(tr("Float format - all"), this, SLOT(setFloatAll()));
+    } else {
+        /*
+         *      If the column is odd, then this is a register value which
+         *      can be used to create a variable with the address specified
+         *      by the register.  This is only important for assembly.
+         */
+        menu.addAction(tr("Define a variable with this address"), this,
+            SLOT(defineVariableByAddress()));
+    }
+    menu.exec(QCursor::pos());
+}
+
+void FrameWindow::defineVariableByAddress()
+{
+    QString address = table->currentItem()->text();
+    DefineVariableDialog *dialog = new DefineVariableDialog;
+    dialog->addressEdit->setText(address);
+    if (dialog->exec()) {
+        dialog->result.append("");
+        dialog->result.append("");
+        //emit sendVariableDefinition(dialog->result);
+    }
+    delete dialog;
+}
+
+/*
+ *  Change the current table item's format to decimal.
+ */
+void FrameWindow::setDecimal()
+{
+    int row = table->currentRow();
+    FrameItem *item = (FrameItem *)table->item(row,1);
+    item->setFormat("decimal");
+    item->setText(item->value());
+    table->resizeColumnsToContents();
+}
+
+/*
+ *  Change the current table item's format to hexadecimal.
+ */
+void FrameWindow::setHex()
+{
+    int row = table->currentRow();
+    FrameItem *item = (FrameItem *)table->item(row,1);
+    item->setFormat("hexdecimal");
+    item->setText(item->value());
+    table->resizeColumnsToContents();
+}
+
+/*
+ *  Change all the register's formats to decimal.
+ */
+void FrameWindow::setDecimalAll()
+{
+    FrameItem *item;
+    for ( int i = 0; i < rows; i++ ) {
+        item = (FrameItem *)table->item(i,1);
+        item->setFormat("decimal");
+        item->setText(item->value());
+    }
+    table->resizeColumnsToContents();
+}
+
+/*
+ *  Change all the register's formats to hexadecimal.
+ */
+void FrameWindow::setHexAll()
+{
+    FrameItem *item;
+    for ( int i = 0; i < rows; i++ ) {
+        item = (FrameItem *)table->item(i,1);
+        item->setFormat("hexadecimal");
+        item->setText(item->value());
+    }
+    table->resizeColumnsToContents();
+}
+
+void FrameWindow::setDouble()
+{
+    int row = table->currentRow();
+    FrameItem *item = (FrameItem *)table->item(row,1);
+    item->setFormat("double");
+    item->setText(item->value());
+    table->resizeColumnsToContents();
+}
+
+void FrameWindow::setFloat()
+{
+    int row = table->currentRow();
+    FrameItem *item = (FrameItem *)table->item(row,1);
+    item->setFormat("float");
+    item->setText(item->value());
+    table->resizeColumnsToContents();
+}
+
+void FrameWindow::setDoubleAll()
+{
+    FrameItem *item;
+    for ( int i = 0; i < rows; i++ ) {
+        item = (FrameItem *)table->item(i,1);
+        item->setFormat("double");
+        item->setText(item->value());
+    }
+    table->resizeColumnsToContents();
+}
+
+void FrameWindow::setFloatAll()
+{
+    FrameItem *item;
+    for ( int i = 0; i < rows; i++ ) {
+        item = (FrameItem *)table->item(i,1);
+        item->setFormat("float");
+        item->setText(item->value());
+    }
+    table->resizeColumnsToContents();
+}
+
+void FrameWindow::nextLine ( QString file, int line )
+{
+    FileLine fl;
+    fl.file = file;
+    fl.line = line;
+    FrameLimit *lim = frameLimits[fl];
+
+    if ( lim ) {
+        if ( lim  != limit ) {
+            limit = lim;
+            rebuildTable();
+        }
+        emit requestStack(rows);
+    } else {
+        limit = 0;
+        rows = 0;
+        table->setRowCount(0);
+    }
+}
+
+/*
+ *  Constructor
+ *  
+ *  The parameter is the register's name.
+ */
+FrameItem::FrameItem()
+    : QTableWidgetItem("")
+{
+    format = "hexadecimal";
+    _value.u8 = 0;
+}
+
+/*
+ *  Set the value of this FrameItem
+ */
+void FrameItem::setValue(uLong v)
+{
+    _value.u8 = v;
+}
+
+/*
+ *  Set the format of this FrameItem
+ */
+void FrameItem::setFormat(QString f)
+{
+    format = f;
+}
+
+/*
+ *  Get the value of this FrameItem as it should look in the table
+ */
+QString FrameItem::value()
+{
+    if (format == "decimal") {
+        return QString::number(_value.i8,10);
+    } else if (format == "double" ) {
+        return QString::number(_value.f8);
+    } else if ( format == "float" ) {
+        return QString::number(_value.f4);
+    } else {
+        return QString("0x") + QString::number(_value.u8,16);
+    }
+}
---
+++ framewindow.h
@@ -0,0 +1,283 @@
+#ifndef FRAMEWINDOW_H
+#define FRAMEWINDOW_H
+
+/*
+ RegisterWindow class
+ For display of general purpose registers, no public functions
+ for set and get registers just yet
+ */
+
+#include "types.h"
+#include <QFrame>
+#include <QTableWidget>
+#include <QTableWidgetItem>
+#include <QVBoxLayout>
+#include <QStringList>
+#include <QScrollArea>
+#include <QSize>
+
+class FrameLimit
+{
+public:
+    FrameLimit ( int _currPars=0, int _locals=0, int _newPars=0 );
+    int currPars;
+    int locals;
+    int newPars;
+};
+
+/**
+ *  \class FrameItem
+ *
+ *  \brief Class to store and format the value for an item in a stack frame
+ *
+ *  The Register class stores the value of a general purpose register as a
+ *  QString which is a hexadecimal representation starting with "0x".
+ *
+ *  In addition to storing the value of a register, a Register object stores
+ *  the register's name.  This is used when formatting rip and eflags which
+ *  should not be converted to decimal.
+ *
+ *  A Register has a format which is also a QString.  Presently the format
+ *  should be either "decimal" or "hexadecimal".
+ *
+ *  It contains functions to set the value, set the format, and retrieve the
+ *  value formatted for display.
+ */
+class FrameItem: public QTableWidgetItem
+{
+public:
+    /**
+     *  Constructor for the FrameItem class
+     *
+     *  \param name The name of the FrameItem
+     */
+    FrameItem();
+
+    /**
+     *  \fn value
+     *
+     *  Function to retrieve the value of the FrameItem formatted
+     *  according to the format value in the FrameItem
+     *
+     *  \return Formatted value as a QString
+     */
+    QString value();
+
+    AllTypes _value;
+
+    /**
+     *  \fn setValue
+     *
+     *  Function to set the value of a FrameItem
+     *
+     *  \param value A QString containing the value of the quadword.
+     */
+    void setValue(unsigned long value);
+
+
+    /**
+     *  \fn setFormat
+     *
+     *  Function to set the format for a FrameItem.
+     *
+     *  \param format A QString holding the desired quadword format
+     */
+    void setFormat(QString format);
+
+    QString format;     ///< The format for displaying the Register
+};
+
+/**
+ *  \class FrameWindow
+ *
+ *  \brief Class to display the current stack frame
+ *
+ *  The RegisterWindow class is a derived class of QFrame which is a
+ *  displayable widget of the Qt collection.  The register window in ebe
+ *  displays the general purpose register in a QTableWidget and allows the
+ *  user to select the format for either a single register or all the
+ *  registers using a popup menu.
+ *
+ *  Internally registers are stored using a QHash which uses the register
+ *  name as the key and a pointer to a Register object as the value for a key.
+ *  To facilitate setting the proper item in the table, there is a second
+ *  QHash which holds a pointer to a QTableWidgetItem.  This allows rapid
+ *  access to the register's value and the place to store the value in
+ *  the table.
+ *
+ *  The registers are received by the RegisterWindow class using the
+ *  receiveRegs slot.  This function has a single parameter, a QHash
+ *  containing register values indexed by their names.
+ *
+ *  If the user uses a right click in the register window the
+ *  contextMenuEvent function is called.  This is a virtual function
+ *  defined in the QFrame class.  This function builds a popup menu depending
+ *  on whether the click was on a register name or its value.  If the
+ *  click was on a register name, the user is presented with a popup
+ *  menu allowing the user to set the format for that register or for all
+ *  the registers.
+ *
+ *  The register window's setFontHeightAndWidth function is called from
+ *  the main window whenever the user increases or decreases the font size.
+ *  The font height and width values are retained for later internal calls
+ *  the font adjustment function when register values change.
+ */
+class FrameWindow: public QFrame
+{
+    Q_OBJECT
+
+public:
+    /**
+     *  Constructor for the FrameWindow class
+     *
+     *  The constructor calls the QFrame constructor before its own
+     *  initialization.  Then it sets the appearance of the frame.
+     *
+     *  The only widget displayed in the FrameWindow is a QTableWidget
+     *  which is created in the constructor and initialized with "0" values
+     *  for all the registers.  At this time it sets the QTableWidgetItem
+     *  pointers in the registerMap for convenient access when receiving
+     *  registers from gdb.
+     *
+     *  The constructor initializes the QStringList namesList with the
+     *  names of all the registers for later convenience.  Then it creates
+     *  a Register object for each register and stores these pointers in
+     *  the regs map.
+     */
+    FrameWindow(QWidget *parent = 0);
+
+    FrameLimit *limit;
+    int rows;
+    QVBoxLayout *layout;
+    int returnRow;
+    int local1Row;
+
+    void buildTable();
+    void rebuildTable();
+
+    /**
+     *  \fn setFontHeightAndWidth
+     *
+     *  This function sets the height of each row in the table to the
+     *  height passed into the function plus 3.  Next it processes each
+     *  column finding the maximum length string displayed in the column.
+     *  Then it sets the width of the column based on the maximum width
+     *  and the font width passed into the function.
+     *
+     *  \param height The height in pixels for the fixed with font
+     *  \param width The width in pixels for the fixed width font
+     */
+    void setFontHeightAndWidth(int height, int width);
+
+    /**
+     *  \fn setRegister
+     *
+     *  This function sets the values of a single named register.
+     *
+     *  \param name The name of the register to set
+     *  \param value The new value for the register
+     */
+    void setValue(QString name, QString value);
+
+    /**
+     *  QHash which provides a register pointer based on a register
+     *  name as key.
+     */
+    QHash<QString, FrameItem *> items;
+
+    /**
+     *  QTableWidget pointer to the table displayed in the RegisterWindow.
+     */
+    QTableWidget *table;
+
+    /**
+     *  \fn sizeHint
+     *
+     *  This function is called by Qt when it needs to determine how much
+     *  space a widget requires.  For the register window we return a
+     *  size of 300 columns and 50 rows.
+     *
+     *  \return The suggested size for the RegisterWindow
+     */
+    QSize sizeHint() const;
+
+    int fontWidth;          ///< The width of a fixed width character
+    int fontHeight;         ///< The height of a fixed width character
+
+    /**
+     *  \fn contextMenuEvent
+     *
+     *  This function is called when the user right clicks in the RegisterWindow.
+     *  If the click happens on a name the user is presented with a popup menu
+     *  allowing changing the format for that particular register or changing the
+     *  format for all the registers.
+     *
+     *  If the click happens on a register value, the user will be presented
+     *  with a dialog to define a user-defined variable with the address
+     *  specified by the register's value.
+     *
+     *  \param event A pointer to a event object
+     */
+    void contextMenuEvent(QContextMenuEvent *event);
+
+    void nextLine(QString file, int line);
+
+signals:
+    void requestStack(int rows);
+public slots:
+
+    void receiveStack(QStringList results);
+
+    void defineVariableByAddress();
+
+    /**
+     *  \fn receiveRegs
+     *
+     *  This slot is called when the gdb object has a new set of register
+     *  values to be displayed.  The namesList array is used to cycle
+     *  through the names of the registers to set their values.  It calls
+     *  setFontHeightAndWidth to resize the rows and columns of the table
+     *  based on the latest register contents.
+     */
+    void receiveFrame(StringHash);
+
+    /**
+     *  \fn setDecimal
+     *
+     *  This function is called when the popup menu action is to set the
+     *  format for a single register to "decimal".
+     */
+    void setDecimal();
+
+    /**
+     *  \fn setHex
+     *
+     *  This function is called when the popup menu action is to set the
+     *  format for a single register to "hexadecimal".
+     */
+    void setHex();
+
+    /**
+     *  \fn setDecimalAll
+     *
+     *  This function is called when the popup menu action is to set the
+     *  format for all registers to "decimal".
+     */
+    void setDecimalAll();
+
+    /**
+     *  \fn setHexAll
+     *
+     *  This function is called when the popup menu action is to set the
+     *  format for all registers to "hexadecimal".
+     */
+    void setHexAll();
+    void setFloat();
+    void setDouble();
+    void setFloatAll();
+    void setDoubleAll();
+
+};
+
+#endif
+
